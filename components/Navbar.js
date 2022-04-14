24749dba3d25649ea6aeb5e20d6e4bcac2818cde
tex111 <55551426+tex111@users.noreply.github.com>
2022-04-13 22:30:42 -0500

    4/13/22 2230

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 
@@ -11,10 +10,10 @@ const Navbar = () => {
   const [showNavMenu, setShowNavMenu] = useState(false);
 
   function CheckActive(linkRoute) {
-    let result = "mr-4 text-gray-400";
+    let result = "";
 
-    if (router.pathname === linkRoute) {
-      result = "mr-4";
+    if (router.pathname != linkRoute) {
+      result = "text-gray-400 hover:text-blue-600 ";
     }
 
     return result;
@@ -22,30 +21,86 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 w-screen h-auto mb-4 bg-slate-900">
-      <div className="flex flex-wrap px-4 py-4 md:px-8 items-center text-white">
+      <div className="flex flex-col px-4 py-4 md:px-8 font-semibold text-white text-lg">
         <div className="flex w-full justify-between">
           <div className="flex">
-            <div className="mr-8 text-xl whitespace-nowrap">Aether Aeon</div>
-            <div className="hidden md:flex">
+            <div className="mr-16 text-xl whitespace-nowrap self-center">
+              Aether Aeon
+            </div>
+            <div className="hidden md:flex self-center">
               <Link href="/">
-                <a className={CheckActive("/")}>Home</a>
+                <a className={CheckActive("/") + "mr-4"}>Home</a>
               </Link>
               <Link href="/news">
-                <a className={CheckActive("/news")}>News</a>
+                <a className={CheckActive("/news") + "mr-4"}>News</a>
               </Link>
               <Link href="/about">
-                <a className={CheckActive("/about")}>About</a>
+                <a className={CheckActive("/about") + "mr-4"}>About</a>
               </Link>
               <Link href="/wiki">
-                <a className={CheckActive("/wiki/[[...params]]")}>Wiki</a>
+                <a className={CheckActive("/wiki/[[...params]]") + "mr-4"}>
+                  Wiki
+                </a>
               </Link>
             </div>
           </div>
-          <div className="flex">
-            <button className="md:hidden flex items-center px-2 py-2 border rounded border-white">
-              <FaBars />
+          <div className="hidden md:flex w-1/4">
+            <input
+              type="text"
+              className="hidden md:flex px-2 py-1 pl-4 mr-4 w-full rounded border
+              placeholder:text-slate-300 bg-slate-700 border-white 
+              focus:ring-blue-600 focus:border-blue-600"
+              placeholder="Search Wiki..."
+            />
+            <button
+              type="button"
+              className="hidden md:flex px-4 border rounded
+              border-white hover:bg-blue-600"
+            >
+              <span className="self-center">Search</span>
             </button>
-            <div className="hidden md:flex">Searchbar</div>
+          </div>
+          <button
+            type="button"
+            className="flex md:hidden items-center px-2 py-2 border rounded border-white"
+          >
+            <FaBars />
+          </button>
+        </div>
+        <div className="flex flex-col md:hidden">
+          <Link href="/">
+            <a className={CheckActive("/") + "mt-4 self-center"}>Home</a>
+          </Link>
+          <Link href="/news">
+            <a className={CheckActive("/news") + "mt-2 self-center"}>News</a>
+          </Link>
+          <Link href="/about">
+            <a className={CheckActive("/about") + "mt-2 self-center"}>About</a>
+          </Link>
+          <Link href="/wiki">
+            <a
+              className={
+                CheckActive("/wiki/[[...params]]") + "mt-2 self-center"
+              }
+            >
+              Wiki
+            </a>
+          </Link>
+          <div className="flex mt-4">
+            <input
+              type="text"
+              className="flex px-2 py-1 pl-4 mr-4 grow rounded border
+              placeholder:text-slate-300 bg-slate-700 border-white 
+              focus:ring-blue-600 focus:border-blue-600"
+              placeholder="Search Wiki..."
+            />
+            <button
+              type="button"
+              className="flex px-4 border rounded
+              border-white hover:bg-blue-600"
+            >
+              <span className="self-center">Search</span>
+            </button>
           </div>
         </div>
       </div>
